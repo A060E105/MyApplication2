f6489bebf14044df2ffd4426169b565b5e65ccf3
Jin-Mo,Lin <s106003041@g.ksu.edu.tw>
2020-04-17 11:38:25 +0800

    update activity_main.xml and activity_main2.xml

--- app/src/main/java/com/example/i5302/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/i5302/myapplication2/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.i5302.myapplication2
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
