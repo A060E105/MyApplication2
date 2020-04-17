6588a7400158a5e3969b12ffbe03365c82e4cc1d
Jin-Mo,Lin <s106003041@g.ksu.edu.tw>
2020-04-17 12:09:42 +0800

    add program

--- app/src/main/java/com/example/i5302/myapplication2/Main2Activity.kt
+++ app/src/main/java/com/example/i5302/myapplication2/Main2Activity.kt
@@ -2,14 +2,25 @@ package com.example.i5302.myapplication2
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        intent?.extras?.let {
-            val data = it.getInt("key")
+
+        btn_send.setOnClickListener {
+            if (ed_drink.length() < 1) {
+                Toast.makeText(this, "請輸入飲料名稱", Toast.LENGTH_SHORT).show()
+            } else {
+                val b = Bundle()
+
+                b.putString("drink", ed_drink.text.toString())
+                b.putString("sweet", radioGroup.findViewById<RadioButton>(radioGroup.checkedRadioButtonId).text.toString())
+            }
         }
     }
 }
--- app/src/main/java/com/example/i5302/myapplication2/MainActivity.kt
+++ app/src/main/java/com/example/i5302/myapplication2/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.i5302.myapplication2
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, MainActivity::class.java)
-        intent.putExtra("key", 123)
-        startActivity(intent)
+
+        btn_choice.setOnClickListener {
+            startActivityForResult(Intent(this, Main2Activity::class.java), 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        data?.extras?.let {
+            if (requestCode==1 && resultCode== Activity.RESULT_OK) {
+                tv_drink.text = "飲料： ${it.getString("drink")}"
+                tv_sweet.text = "甜度： ${it.getString("sweet")}"
+                tv_ice.text = "冰塊： ${it.getString("ice")}"
+            }
+        }
     }
 }
